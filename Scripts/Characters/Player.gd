7a1dc8de7d9241802654b6003ec4ceb93d0a9281
Shafigullin Mikhail <mikeice414@gmail.com>
2023-12-05 21:49:12 +0300

    start to work with player controller

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -6,12 +6,22 @@ var snap_vector : Vector3 = Vector3.DOWN
 var speed : float
 var busy : bool = false
 
+@export var is_new_movement : bool = false;
 
-@export_group("Movement variables")
+@export_group("Movement variables DEPRECATED")
 @export var walk_speed : float = 2.0
 @export var run_speed : float = 5.0
 @export var jump_strength : float = 15.0
 @export var gravity : float = 50.0
+
+@export_group("Movement variables")
+@export var jump_height: float = 2.5
+@export var time_in_air: float = 0.3
+@export var walk_speed_new : float = 2.0
+@export var run_speed_new : float = 5.0
+
+var start_jump_velocity = (2 * jump_height) / time_in_air
+var gravity_counted = (-2 * jump_height) / (time_in_air * time_in_air)
 
 const ANIMATION_BLEND : float = 7.0
 
@@ -24,6 +34,51 @@ func _ready():
 	Global.player = self
 
 func _physics_process(delta):
+	if is_new_movement:
+		physics_process_new(delta);
+	else:
+		physics_process_depricated(delta);
+	 
+func physics_process_new(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	var move_direction : Vector3 = Vector3.ZERO
+	if not busy:
+		move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		move_direction.z = Input.get_action_strength("move_backwards") - Input.get_action_strength("move_forwards")
+		move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+		
+		var just_landed := is_on_floor() and snap_vector == Vector3.ZERO
+		var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
+		if is_jumping:
+			velocity.y = start_jump_velocity
+			snap_vector = Vector3.ZERO
+		elif just_landed:
+			snap_vector = Vector3.DOWN
+	
+	if Input.is_action_pressed("run"):
+		speed = run_speed
+	else:
+		speed = walk_speed
+	
+	velocity.x = move_direction.x * speed
+	velocity.z = move_direction.z * speed
+	velocity.y += gravity_counted * delta
+	
+	print('velocity', velocity.y);
+	
+	if move_direction:
+		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
+	
+	apply_floor_snap()
+	move_and_slide()
+	animate(delta)
+	
+func physics_process_depricated(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
