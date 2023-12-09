b2f379f28074b23a807cfd67bef15537874ab775
Shafigullin Mikhail <mikeice414@gmail.com>
2023-12-09 16:45:09 +0300

    add buffered and coyote jump

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -15,14 +15,20 @@ var busy : bool = false
 @export var gravity : float = 50.0
 
 @export_group("Movement variables")
-@export var planned_jump_height: float = 2.0
-@export var planned_time_in_air: float = 0.3
+@export var planned_jump_height: float = 2.5
+@export var planned_time_in_air: float = 0.4
 @export var walk_speed_new : float = 2.0
 @export var run_speed_new : float = 5.0
-@export var is_double_jump_available : bool = true;
+@export var max_available_jumps : int = 2;
 @export var time_block_jump_after_jumping: float = 0.05
+@export var coyote_time: float = 0.2
+@export var jump_buffer_time: float = 0.2
 
 var time_in_air: float = 0
+var jump_counter: int = 0;
+var is_jump_pressed: bool = false;
+var coyote_time_ended: bool = false;
+var jump_buffer_timer: Timer = Timer.new();
 
 var start_jump_velocity:float = (2 * planned_jump_height) / planned_time_in_air
 var gravity_counted:float = (-2 * planned_jump_height) / (planned_time_in_air * planned_time_in_air)
@@ -42,16 +48,15 @@ const ANIMATION_BLEND : float = 7.0
 
 func _ready():
 	Global.player = self
+	add_child(jump_buffer_timer)
+	jump_buffer_timer.wait_time = jump_buffer_time
+	jump_buffer_timer.one_shot = true
 
 func _physics_process(delta):
-	if is_new_movement:
-		physics_process_new(delta);
-	else:
-		physics_process_deprecated(delta);
+	physics_process_new(delta);
 		
 func _process(delta):
-	if is_new_movement:
-		process_new(delta);
+	process_new(delta);
 		
 func _input(event):
 	if currentState == PlayerState.STATE_ON_GROUND:
@@ -103,7 +108,7 @@ func physics_process_new(delta):
 
 func gravity_evaluate(velocity : Vector3):
 	if velocity.y <= 0: 
-		return gravity_counted * 0.5;
+		return gravity_counted * 1.2;
 	else: 
 		return gravity_counted;
 		
@@ -111,20 +116,28 @@ func gravity_evaluate(velocity : Vector3):
 func handle_input_standing(input: InputEvent):
 	if input.is_action_pressed("jump") and is_on_floor():
 		#print('standing jump', is_double_jump_available)
+		is_jump_pressed = true
 		velocity.y = start_jump_velocity
 		changeState(PlayerState.STATE_IN_AIR)
-		is_double_jump_available = true
+		jump_counter+=1;
+		jump_buffer_timer.start();
 	
 	
 func handle_input_jumping(input: InputEvent):
-	if input.is_action_pressed("jump") and is_double_jump_available and time_in_air > time_block_jump_after_jumping:
-		#print('jumping jump', is_double_jump_available)
-		is_double_jump_available = false;
-		velocity.y = start_jump_velocity
+	if (input.is_action_pressed("jump")):
+		jump_buffer_timer.start();
+		if (jump_counter <= max_available_jumps and time_in_air > time_block_jump_after_jumping):
+			jump();
+	
 		
 func update_standing(delta):
 	time_in_air = 0;
-	is_double_jump_available = true;
+	jump_counter = 0;
+	is_jump_pressed = false;
+	coyote_time_ended = false;
+	if !jump_buffer_timer.is_stopped():
+		print('jump_buffered is activated')
+		jump();
 	if !is_on_floor(): 
 		changeState(PlayerState.STATE_IN_AIR)
 	
@@ -133,48 +146,18 @@ func update_in_air(delta):
 	if time_in_air > time_block_jump_after_jumping:
 		if is_on_floor(): 
 			changeState(PlayerState.STATE_ON_GROUND)
-			
-func changeState(state: PlayerState):
-	print('current state: ', state)
-	currentState = state;
-	
-func physics_process_deprecated(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		else:
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	var move_direction : Vector3 = Vector3.ZERO
-	if not busy:
-		move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-		move_direction.z = Input.get_action_strength("move_backwards") - Input.get_action_strength("move_forwards")
-		move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+	if time_in_air > coyote_time and !coyote_time_ended and !is_jump_pressed:
+		coyote_time_ended = true
+		jump_counter+=1
 
-		var just_landed := is_on_floor() and snap_vector == Vector3.ZERO
-		var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
-		if is_jumping:
-			velocity.y = jump_strength
-			snap_vector = Vector3.ZERO
-		elif just_landed:
-			snap_vector = Vector3.DOWN
-	
-	velocity.y -= gravity * delta
-	
-	if Input.is_action_pressed("run"):
-		speed = run_speed
-	else:
-		speed = walk_speed
-	
-	velocity.x = move_direction.x * speed
-	velocity.z = move_direction.z * speed
-	
-	if move_direction:
-		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
-	
-	apply_floor_snap()
-	move_and_slide()
-	animate(delta)
+func jump():
+	is_jump_pressed = true;
+	jump_counter+=1;
+	velocity.y = start_jump_velocity
+
+func changeState(state: PlayerState):
+	#print('current state: ', state)
+	currentState = state;
 
 func animate(delta):
 	if is_on_floor():
