a1b2005ee31c4d61e4549d5c1de8f8e8c7ce7e0b
nn <86272232+loop-nop@users.noreply.github.com>
2023-12-11 17:44:50 +0000

    failed test

--- Assets/Test/testAnim.gd
+++ Assets/Test/testAnim.gd
@@ -1,7 +1,8 @@
 extends Node3D
-
+@onready var skel: Skeleton3D = $SizedTim/TrueSizeTim/Skeleton3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_on_usable_object_on_object_use()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -9,6 +10,4 @@ func _process(delta):
 	pass
 
 func _on_usable_object_on_object_use():
-	var skel: Skeleton3D = $SizedTim/TrueSizeTim/Skeleton3D
-	skel.physical_bones_start_simulation()
-	pass # Replace with function body.
+	skel.physical_bones_start_simulation(["mixamorig_Neck","mixamorig_Spine","mixamorig_Spine1","mixamorig_Spine2","mixamorig_LeftShoulder","mixamorig_LeftArm","mixamorig_LeftForeArm","mixamorig_LeftHand","mixamorig_RightShoulder","mixamorig_RightArm","mixamorig_RightForeArm"])
